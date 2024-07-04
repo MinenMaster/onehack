250e81610c527882c8ffc58ea56ec094d096f17d
Dom <95213203+MinenMaster@users.noreply.github.com>
2024-07-04 10:53:27 +0200

    added DrawContext

--- src/main/java/com/brodi/onehack/mixins/inGameHudMixin.java
+++ src/main/java/com/brodi/onehack/mixins/inGameHudMixin.java
@@ -1,8 +1,8 @@
 package com.brodi.onehack.mixins;
 
 import com.brodi.onehack.ui.Hud;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class inGameHudMixin {
 
     @Inject(method = "render", at = @At("RETURN"), cancellable = true)
-    public void renderHud(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-        Hud.render(matrices, tickDelta);
+    public void renderHud(DrawContext context, float tickDelta, CallbackInfo ci) {
+        Hud.render(context, tickDelta);
     }
 }
