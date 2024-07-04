cc22f5340d541fc08f373d95ce9132bb522735f0
Dom <95213203+MinenMaster@users.noreply.github.com>
2024-07-04 12:47:08 +0200

    Update Hud.java

--- src/main/java/com/brodi/onehack/ui/Hud.java
+++ src/main/java/com/brodi/onehack/ui/Hud.java
@@ -12,4 +12,18 @@ public class Hud {
         context.drawText(mc.textRenderer, "OneHack" , 10, 10, -1, true);
         renderArrayList(context);
     }
+
+    public static void renderArrayList(DrawContext context) {
+        int index = 0;
+        int sWidth = mc.getWindow().getScaledWidth();
+
+        List<Mod> enabledModules = ModuleManager.INSTANCE.getEnabledModules();
+
+        enabledModules.sort(Comparator.comparingInt(m -> mc.textRenderer.getWidth(((Mod)m).getDisplayName())).reversed());
+
+        for (Mod mod : enabledModules) {
+            context.drawText(mc.textRenderer, mod.getDisplayName(), (sWidth - 4) - mc.textRenderer.getWidth(mod.getDisplayName()), 10 + (index * mc.textRenderer.fontHeight), -1, true);
+            index++;
+        }
+    }
 }
